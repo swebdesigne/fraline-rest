220f20b0b0cd7501c01ee0f3e7888131824a7b0a
Sivolobov Igor <swebdesigne@gmail.com>
2025-12-06 21:31:26 +0300

    init project

---
+++ src/main/java/ru/fraline/FralineRestApplication.java
@@ -0,0 +1,13 @@
+package ru.fraline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FralineRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FralineRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/fraline/FralineRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fraline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FralineRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
